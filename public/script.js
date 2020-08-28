214349e1c0676f61ef15d031bf8f42108034ecd9
Gabriel Barbosa Seibt da Silva <gabriel.barbosa.seibt@gmail.com>
2020-08-28 11:42:12 -0300

    Atualizações

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   path: '/peerjs', 
   host: '/',
-  port: '3000',
+  port: '443',
   config: {'iceServers': [
     { url: 'stun:stun.l.google.com:19302' },
     { url: 'turn:192.158.29.39:3478?transport=udp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username:'28224511:1379330808' },
